128b0ce3d1828aeb29265e49dce30b5e5dd17c98
Gaudéric PLEDRAN <gpledran@gmail.com>
2016-04-18 21:46:25 +0200

    Added android app files

---
+++ app/src/main/java/fr/gpledran/bicloo/MapsActivity.java
@@ -0,0 +1,53 @@
+package fr.gpledran.bicloo;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng nantes = new LatLng(47.20532409029517, -1.555183107916184);
+        mMap.addMarker(new MarkerOptions().position(nantes).title("Nantes"));
+
+        LatLng bellamy = new LatLng(47.222902006053054, -1.557229797031573);
+        mMap.addMarker(new MarkerOptions().position(bellamy).title("BELLAMY"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(nantes, 12.0f));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(nantes));
+    }
+}
