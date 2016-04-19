f2fefb01cd73b0252c3cc94305787173d655802a
Gaudéric PLEDRAN <gpledran@gmail.com>
2016-04-19 22:16:49 +0200

    Added Material Toolbar & colors

--- app/src/main/java/fr/gpledran/bicloo/MapsActivity.java
+++ app/src/main/java/fr/gpledran/bicloo/MapsActivity.java
@@ -1,9 +1,15 @@
 package fr.gpledran.bicloo;
 
-import android.location.Location;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,7 +32,7 @@ import retrofit.RetrofitError;
 import retrofit.client.Response;
 import retrofit.converter.GsonConverter;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap map;
     private List<Station> stationList;
@@ -40,6 +46,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
     /**
@@ -55,10 +65,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
 
-//        // Add a marker in Nantes and move the camera
-//        LatLng nantes = new LatLng(47.20532409029517, -1.555183107916184);
-//        map.addMarker(new MarkerOptions().position(nantes).title("Nantes"));
-//
         // Center camera on Nantes
         LatLng nantes = new LatLng(47.2185256, -1.55408);
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(nantes, 15.0f));
@@ -94,5 +100,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Log.d("JCDecaux API ERROR" , "Error : " + error.toString());
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
